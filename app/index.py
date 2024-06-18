d3b5e684a916e0124085c2de6015a2ac22a51771
silvermete0r <gregormelon.2003@gmail.com>
2024-06-18 23:03:40 +0500

    vercel-running

---
+++ app/index.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from app.complexity import get_complexity_analysis
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    complexity = None
+
+    if request.method == 'POST':
+        code = request.form['code']
+        complexity = get_complexity_analysis(code)
+    
+    print(complexity)
+
+    return render_template('index.html', complexity=complexity)
+
+@app.route('/stars')
+def hall_of_fame():
+    return render_template('stars.html')
+
+if __name__ == '__main__':
+    app.run()
