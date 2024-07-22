ef8a9816ad71345398a5df3574c37ff9204c3947
silvermete0r <gregormelon.2003@gmail.com>
2024-07-22 14:04:19 +0500

    yandex-analytics-added

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from app.complexity import get_complexity_analysis
 from app import app
+import json
 
 limiter = Limiter(
     get_remote_address,
@@ -18,11 +19,11 @@ def index():
     if request.method == 'POST':
         code = request.form['code']
         complexity = get_complexity_analysis(code)
-    
-    print(complexity)
 
-    return render_template('index.html', complexity=complexity)
+    return render_template('index.html', complexity = complexity)
 
-@app.route('/stars')
+@app.route('/stars', methods=['GET'])
 def hall_of_fame():
-    return render_template('stars.html')
+    with open('stars.json', 'r') as f:
+        stars = json.load(f)
+    return render_template('stars.html', stars = stars)
