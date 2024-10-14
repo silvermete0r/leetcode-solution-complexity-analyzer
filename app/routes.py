1f055a713bf8bfcf67c263d6a8c04f16af1fd3a5
silvermete0r <gregormelon.2003@gmail.com>
2024-10-15 00:07:48 +0500

    leetcode-kz-14-oct-update

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,19 @@
 from flask import render_template, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_caching import Cache
 from app.complexity import get_complexity_analysis
+from app.utils.leetcode import get_leetcode_user_stats
 from app import app
 import json
+
+config = {
+    'DEBUG': True,
+    'CACHE_TYPE': 'SimpleCache',
+    'CACHE_DEFAULT_TIMEOUT': 300
+}
+
+cache = Cache(config=config)
 
 limiter = Limiter(
     get_remote_address,
@@ -27,3 +37,9 @@ def hall_of_fame():
     with open('stars.json', 'r') as f:
         stars = json.load(f)
     return render_template('stars.html', stars = stars)
+ 
+@cache.cached(timeout=300)
+@app.route('/profile/<username>', methods=['GET'])
+def leetcode_profile(username):
+    profile_data = get_leetcode_user_stats(username)
+    return render_template('profile.html', profile_data = profile_data)
---
+++ app/utils/leetcode.py
@@ -0,0 +1,136 @@
+import requests
+
+
+def get_leetcode_user_stats(username: str) -> dict:
+    data = {"status": "error"}
+    try:
+        url = 'https://leetcode.com/graphql'
+        query = """
+        query getUserStats($username: String!) {
+          allQuestionsCount {
+            difficulty
+            count
+          }
+          matchedUser(username: $username) {
+            username
+            githubUrl
+            twitterUrl
+            linkedinUrl
+            profile {
+              reputation
+              ranking
+              userAvatar
+              realName
+              countryName
+              company
+              jobTitle
+              skillTags
+              postViewCount
+            }
+            contributions {
+              points
+            }
+            submitStatsGlobal {
+              acSubmissionNum {
+                difficulty
+                count
+              }
+            }
+          }
+        }
+      """
+        variables = {"username": username}
+        response = requests.post(url,
+                                 json={
+                                     "query": query,
+                                     "variables": variables
+                                 })
+        if response.json().get(
+                "errors") is not None or response.status_code != 200:
+            return {"status": "error"}
+        data = response.json()["data"]
+        total_solved = data["matchedUser"]["submitStatsGlobal"]["acSubmissionNum"][
+            0]["count"]
+        total_questions = data["allQuestionsCount"][0]["count"]
+        easy_solved = data["matchedUser"]["submitStatsGlobal"]["acSubmissionNum"][
+            1]["count"]
+        total_easy = data["allQuestionsCount"][1]["count"]
+        medium_solved = data["matchedUser"]["submitStatsGlobal"][
+            "acSubmissionNum"][2]["count"]
+        total_medium = data["allQuestionsCount"][2]["count"]
+        hard_solved = data["matchedUser"]["submitStatsGlobal"]["acSubmissionNum"][
+            3]["count"]
+        total_hard = data["allQuestionsCount"][3]["count"]
+        ranking = data["matchedUser"]["profile"]["ranking"]
+        contribution_points = data["matchedUser"]["contributions"]["points"]
+        reputation = data["matchedUser"]["profile"]["reputation"]
+        github_url = data["matchedUser"]["githubUrl"]
+        twitter_url = data["matchedUser"]["twitterUrl"]
+        linkedin_url = data["matchedUser"]["linkedinUrl"]
+        userAvatar = data["matchedUser"]["profile"]["userAvatar"]
+        realName = data["matchedUser"]["profile"]["realName"]
+        countryName = data["matchedUser"]["profile"]["countryName"]
+        company = data["matchedUser"]["profile"]["company"]
+        jobTitle = data["matchedUser"]["profile"]["jobTitle"]
+        skillTags = data["matchedUser"]["profile"]["skillTags"]
+        postViewCount = data["matchedUser"]["profile"]["postViewCount"]
+        best_lang = __get_leetcode_user_planguages(username)
+        data = {
+            "status": "success",
+            "totalSolved": total_solved,
+            "totalQuestions": total_questions,
+            "easySolved": easy_solved,
+            "totalEasy": total_easy,
+            "mediumSolved": medium_solved,
+            "totalMedium": total_medium,
+            "hardSolved": hard_solved,
+            "totalHard": total_hard,
+            "ranking": ranking,
+            "contributionPoints": contribution_points,
+            "reputation": reputation,
+            "githubUrl": github_url,
+            "twitterUrl": twitter_url,
+            "linkedinUrl": linkedin_url,
+            "userAvatar": userAvatar,
+            "realName": realName if realName else 'ANONYMOUS',
+            "countryName": countryName if countryName else 'UNKNOWN',
+            "company": company if company else 'COMPANY',
+            "jobTitle": jobTitle if company else 'SWE',
+            "skillTags": skillTags if skillTags else [best_lang],
+            "postViewCount": postViewCount,
+            "bestLanguage": best_lang
+        }
+    except Exception as e:
+        print("Error in get_leetcode_user_stats: ", e)
+    finally:
+        return data
+
+
+def __get_leetcode_user_planguages(username):
+    url = 'https://leetcode.com/graphql'
+    query = """
+      query languageStats($username: String!) {
+      matchedUser(username: $username) {
+          languageProblemCount {
+          languageName
+          problemsSolved
+          }
+      }
+      }
+      """
+    variables = {"username": username}
+    response = requests.post(
+        url, json={"query": query, "variables": variables})
+    try:
+        if response.json().get(
+                "errors") is not None or response.status_code != 200:
+            return 'Not Found'
+        data = response.json()["data"]["matchedUser"]["languageProblemCount"]
+        return max(data, key=lambda x: x['problemsSolved'])['languageName'].strip()
+    except Exception as e:
+        print(f'Error in get_leetcode_user_planguages: {str(e)}')
+        return 'Not Found'
+
+
+if __name__ == '__main__':
+    print(get_leetcode_user_stats('silvermete0r'))
